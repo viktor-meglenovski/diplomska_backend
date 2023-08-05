77b77ee937eacb5b7e50c5a1fbba0a1e93eb576a
Viktor Meglenovski <viktor.meglenovski@gmail.com>
2023-08-05 16:13:00 +0200

    initial commit

---
+++ src/main/java/com/diplomska_backend/DiplomskaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diplomska_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomskaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomskaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomska_backend/DiplomskaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomska_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomskaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
